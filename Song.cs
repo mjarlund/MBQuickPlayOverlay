777d13c79da453307dbd596379755866997b59ac
Mikkel Jarlund <mikkel@jarlund.xyz>
2016-11-19 02:02:54 +0100

    Added files via upload
    Project files.

---
+++ Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicBeePlugin
+{
+    public class Song : Plugin
+    {
+        private string[] _Data = new string[0];
+
+        public Song(string sourceFileUrl, MusicBeeApiInterface pApi)
+        {
+            Url = sourceFileUrl;
+            TrackTitle = pApi.Library_GetFileTag(Url, MetaDataType.TrackTitle);
+        }
+
+        public string Url { get; }
+        public string TrackTitle { get; }
+    }
+}
